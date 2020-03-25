1c06fd14b099908a36a5946994deb905cd2a3a95
Iris Leal <iris_leal_m@hotmail.com>
2020-03-25 11:12:10 -0700

    Now it renders my favotite books

---
+++ data/books.sql
@@ -0,0 +1,41 @@
+DROP TABLE IF EXISTS myBooks;
+
+CREATE TABLE myBooks (
+  id serial primary key,
+  title VARCHAR(255),
+  author VARCHAR(255),
+  description VARCHAR(5000),
+  isbn VARCHAR(255),
+  image_url VARCHAR(2500),
+  bookShelf VARCHAR(2500)
+);
+
+INSERT INTO myBooks (title,author,description,isbn,image_url,bookShelf) 
+VALUES (
+  'Miranda in Milan',
+  'Katharine Duckett',
+  'With Miranda in Milan, debut author Katharine Duckett reimagines the consequences of Shakespeare’s The Tempest, casting Miranda into a Milanese pit of vipers and building a queer love story that lifts off the page in whirlwinds of feeling. After the tempest, after the reunion, after her father drowned his books, Miranda was meant to enter a brave new world. Naples awaited her, and Ferdinand, and a throne. Instead she finds herself in Milan, in her father’s castle, surrounded by hostile servants who treat her like a ghost. Whispers cling to her like spiderwebs, whispers that carry her dead mother’s name. And though he promised to give away his power, Milan is once again contorting around Prospero’s dark arts. With only Dorothea, her sole companion and confidant to aid her, Miranda must cut through the mystery and find the truth about her father, her mother, and herself. “Love and lust, mothers and monsters, magicians and masked balls, all delivered with Shakespearean panache.” —Nicola Griffith, author of Hild “Miranda in Milan is somehow both utterly charming and perfectly sinister, and altogether delightful. A pleasure for any lover of romance, myth, and magic—whether or not they',
+  '9781250306319',
+  'http://books.google.com/books/content?id=4MhnDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+  'TBD'
+);
+
+INSERT INTO myBooks (title,author,description,isbn,image_url,bookShelf) 
+VALUES (
+  'The Happiness Hypothesis',
+  'Jonathan Haidt',
+  'Explores ten great insights about man, the purpose of life, and happiness selected from diverse traditions and uses current scientific research to question and discuss the ideas.',
+  '0465028012',
+  'http://books.google.com/books/content?id=Tz4wVAp6qL0C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+  'TBD'
+);
+
+INSERT INTO myBooks (title,author,description,isbn,image_url,bookShelf) 
+VALUES (
+  'Cracking the PM Interview',
+  'Gayle Laakmann McDowell',
+  'How many pizzas are delivered in Manhattan? How do you design an alarm clock for the blind? What is your favorite piece of software and why? How would you launch a video rental service in India? This book will teach you how to answer these questions and more. Cracking the PM Interview is a comprehensive book about landing a product management role in a startup or bigger tech company.',
+  '0984782818',
+  'http://books.google.com/books/content?id=vFr9nQEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api',
+  'TBD'
+);
