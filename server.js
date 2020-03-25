0873b1d15a902398bfd663dcc68407b3762c6229
CoreyDeJong <dejongcorey@gmail.com>
2020-03-24 22:58:11 -0700

    able to get the movie images to display

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
 
 
 app.post('/searches', (request, response) => {
-    console.log('cd and li search', request.body);
+    // console.log('cd and li search', request.body);
     // { search: [ '1984', 'title' ] }
     let thingTheyAreSearchingFor = request.body.search[0];
     let titleOrAuthor = request.body.search[1];
@@ -36,28 +36,49 @@ app.post('/searches', (request, response) => {
     } else if(titleOrAuthor === 'author'){
       url += `+inauthor:${thingTheyAreSearchingFor}`;
     }
-    console.log(url)
+    // console.log(url)
 
     superagent.get(url)
         .then(results => {
             let bookArray = results.body.items;
             // console.log(bookArray);
+            console.log(results.body.items[0].volumeInfo)
+
+
             let finalBookArray = bookArray.map(book => {
                 return new Book(book.volumeInfo);
             })
-            console.log(finalBookArray);
+            // console.log(finalBookArray);
             response.render('./pages/searches/show.ejs', {Book: finalBookArray});
         })
 
     });
     
+
+    //lab 11.3.2 - "Prevent mixed content warnings. Resource URLs returned by the API that are  unsecure should be converted to use a secure protocol when the data is processed in the Book constructor." ?????????????????
+
     function Book (obj) {
         const placeholderImage = 'http://i.imgur.com/J5LVHEL.jpg';
-        this.title = obj.title;
-        this.authors = obj.authors;
-        this.description = obj.description;
-        obj.readingModes.image !== null ? this.image = obj.readingModes.image : this.image = placeholderImage;
-    }
+        this.title = (obj.title) ? obj.title : 'Chuck Norris says No';
+        this.authors = (obj.authors[0]) ? obj.authors[0] : 'Iris Leal says she wrote this book';
+        this.description = (obj.description) ? obj.description : 'Corey says you should probably just watch the moview';
+
+        this.image = (obj.imageLinks) ? obj.imageLinks.thumbnail : placeholderImage;
+        // console.log(this.image);
+
+
+//ISBN - same as imageLinks
+// industryIdentifiers:
+//    [ { type: 'ISBN_13', identifier: '9781443816281' },
+//      { type: 'ISBN_10', identifier: '1443816280' } ],
+
+//   imageLinks:
+//    { smallThumbnail:
+//       'http://books.google.com/books/content?id=ThAaBwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
+//      thumbnail:
+//       'http://books.google.com/
+
+}
 
 
 app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
